04ab33718e286f14ddce9083a3615627ebd56615
ebimo21 <ebi4jah15@gmail.com>
2023-05-22 14:23:06 +0100

    add toaster

--- src/components/sections/Track.tsx
+++ src/components/sections/Track.tsx
@@ -1,5 +1,5 @@
 import { getParcel } from '@/config/apiCalls'
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { serverUTCDiff, userUTCDiff } from '@/config/timezone';
 import moment from 'moment';
 import { toast, ToastContainer } from 'react-toastify';
@@ -33,21 +33,27 @@ function Track() {
     ];
     const [parcel, setParcel] = useState(parcelInitial);
     const [tracking_id, setTracking_Id] = useState("");
+    const [fetching, setFetching] = useState(false);
+    const toastPop = useRef<any>(null);
     
     const handleTrackParcel = async(e:any)=>{
         e.preventDefault()
+        setParcel(parcelInitial)
+        setFetching(true)
+        notify("Fetching Parcel Details");
         const response = await getParcel(tracking_id);
         setParcel(response?.data)
         console.log(response?.data ?"true": "false" );
-        if(!response.success){
-            notify();
-            
+        if(response.success){
+            setFetching(false);
+            setTimeout(()=>{
+            toast.dismiss(toastPop.current);
+            }, 500)
             }
     }
 
-    const notify = () => {
-        toast.success("Default Notification !");
-  
+    const notify = (message: string) => {
+        toastPop.current =  toast.success(message);  
       };
 
       const returnTime = (_t: string | null, _serverUTCDiff: number = serverUTCDiff) =>{
@@ -81,8 +87,10 @@ function Track() {
             </form>
         </div>
 
+        {fetching && (<p className='text-center'>Loading...</p>)}
+
         {parcel?.length< 1 && parcel[0]?.id !== null && (<p className='text-center'>Parcel not found</p>)}
-           {parcel?.length> 0 && parcel[0]?.id !== null && 
+           { parcel?.length> 0 && parcel[0]?.id !== null && 
            (
                <div className='md:p-10'>
             <div className='overflow-y-autos m-auto '>
@@ -234,153 +242,7 @@ function Track() {
            )
             }
 
-        <div className='mb-20'>
-        <div className='overflow-y-auto m-auto h-[300px]'>
-           {parcel && <table className='w-screen max-w-5xl font-medium text-left text-sm text-slate-500 '>
-                {parcel.length <1 && (<p className='text-center'>Parcel not found</p>)}
-                <thead>
-                    <tr className='p-2 bg-[#F1F1F1] text-slate-700'>
-                        <th className=' p-4 '>Name</th>
-                        <th className=' p-4  '>Discount Amount</th>
-                    </tr>
-                </thead>
-                {parcel?.map((item, index)=>{
-                    return(
-                        <tbody key={item?.id}>
-                            <tr>
-                                <td className='text-left  p-4 border-b border-b-slate-200  border-b-solid'>
-                                    <span>Tracking ID</span>
-                                </td>
-                                <td className='text-left  p-4 border-b border-b-slate-200  border-b-solid'>
-                                    <span>{item?.Tracking_id}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Status</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.status}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Receiver Email</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.reciever_email}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Reciever Name</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.reciever_name}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Vehicle Type</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.vehicle_type}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Destination</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.destination}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Reciever Address</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.reciever_address}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Receiver Phone</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.reciever_phone}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Route</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.route}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Description</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.description}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Created At</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.created_at}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Last Updated</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.updated_at}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Completed At</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.completed_at}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Sender Name</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.sender_name}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Delivery Date</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.delivery_date}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>Current Location</span>
-                                </td>
-                                <td className=' p-4 border-b border-b-slate-200  border-b-solid '>
-                                    <span>{item?.current_location}</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    )
-                })}
-            </table>}
-        </div>
-        </div>
+        
     </div>
   )
 }
--- src/config/apiCalls.tsx
+++ src/config/apiCalls.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const jscookie = require("jscookie");
-const host = "https://api.ultraexpress.org/";
+const host = "https://node.tradefactory.net/";
 // const host = "http://localhost:3001/";
 
 type loginDetails = {
